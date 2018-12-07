d25e2f87892fbaf31ce5ba77eaf320b87e9d5ca6
Jackson Zou <jzou00@stuy.edu>
2018-12-07 10:49:04 -0500

    wrote bubbleSort() method

--- Sorts.java
+++ Sorts.java
@@ -1,6 +1,10 @@
 import java.util.Random;
 
 public class Sorts{
+  /**Selection sort of an int array.
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
   public static void selectionSort(int[] ary) {
     for (int i = 0; i < ary.length; i++) {
       int index = i;
@@ -17,6 +21,23 @@ public class Sorts{
     }
   }
 
+  /**Bubble sort of an int array.
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static void bubbleSort(int[] data){
+    for (int index = data.length - 1; index > 0; index-=1) {
+      for (int i = 0; i < index; i++) {
+        if (data[i] > data[i+1]) {
+          int hold = data[i];
+          data[i] = data[i+1];
+          data[i+1] = hold;
+        }
+        toString(data);
+      }
+    }
+  }
+
   public static void main(String[] args) {
     Random rand = new Random(); //makes random int array with size determined by input
     int[] array = new int[Integer.parseInt(args[0])];
@@ -24,7 +45,7 @@ public class Sorts{
       array[i] = Math.abs(rand.nextInt() % 100);
     }
     toString(array);
-    Sorts.selectionSort(array);
+    Sorts.bubbleSort(array);
     toString(array);
   }
 
